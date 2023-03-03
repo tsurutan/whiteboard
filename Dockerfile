4587d085b6f1174fc46b2843302e492e302d000f
Atsuhiro <atsuruta@vmware.com>
2023-03-03 19:10:53 +0900

    feat: 🎸 Add basic auth

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ RUN gem install bundler --version=1.1
 RUN gem update --system 1.8.25
 RUN bundle install
 RUN bundle exec rake db:migrate
-CMD bundle exec rails s
+CMD bundle exec unicorn -p 3000 -c ./config/unicorn.rb
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :boxed
+  before_filter :basic_auth
   # before_filter :require_login
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   def require_login
     redirect_to '/auth/google_oauth2' unless session[:logged_in] || AuthorizedIps.authorized_ip?(request.env['HTTP_X_FORWARDED_FOR'])
